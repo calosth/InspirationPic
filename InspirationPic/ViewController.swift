8d9bec94514e49c2065e6069d5b8acc8f504d6d0
Carlos Linares <cadalisa@gmail.com>
2015-03-11 21:49:17 -0430

    Initial Commit

---
+++ InspirationPic/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  InspirationPic
+//
+//  Created by Carlos Linares on 11/3/15.
+//  Copyright (c) 2015 Calosth. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
